47a8635beda164d161b046e55d8458c744307c0b
noskymunzen <munzenmayer@outlook.com>
2023-01-19 16:52:13 -0300

    add genre color constant

---
+++ const/theme.ts
@@ -0,0 +1,15 @@
+import { GenreName } from "../types/anime";
+
+export const GenreColors = {
+  [GenreName.ROMANCE]: "red",
+  [GenreName.SLICE_OF_LIFE]: "green",
+  [GenreName.ADVENTURE]: "yellow",
+  [GenreName.DRAMA]: "teal",
+  [GenreName.COMEDY]: "pink",
+  [GenreName.SPORT]: "telegram",
+  [GenreName.SCI_FI]: "purple",
+  [GenreName.ACTION]: "orange",
+  [GenreName.MYSTERY]: "whatsapp",
+  [GenreName.SUSPENSE]: "gray",
+  [GenreName.SUPERNATURAL]: "cyan",
+}
